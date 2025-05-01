94eaa26e5fd9d6e9a0de21f3f76bbb9305aae16f
Diego Armando Santacruz <diego.santacruz@siesa.com>
2025-05-01 12:03:49 -0500

    CultureServce

---
+++ Services/CultureService.cs
@@ -0,0 +1,38 @@
+using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
+using STZ.Frontend.Services;
+using STZ.Shared.Bases;
+using STZ.Shared.Dtos;
+using STZ.Shared.Entities;
+
+namespace STZ.Frontend.Services;
+
+public class CultureService : ServiceBase<Culture>, ICultureService
+{
+    public CultureService(HttpClient httpClient, IConfiguration configuration) 
+        : base(httpClient, configuration) {}
+
+    public async Task<ResourcesCultureDto> GetResourcesAsync(Guid cultureId)
+    {
+        try
+        {
+            var response =
+                await HttpClient.GetFromJsonAsync<ResourcesCultureDto>($"{Endpoint}/{cultureId.ToString()}/resources");
+
+            if (response is null)
+                throw new Exception("Error al obtener los recursos de la cultura");
+
+            return response;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return new ResourcesCultureDto()
+            {
+                CultureCode = "",
+                Date = DateTime.MinValue,
+                Resources = []
+            };
+        }
+    }
+}
